7f7bba5dff443a1d6059896f956c615a00ab7c68
Daniela <71750294+Daniela280802@users.noreply.github.com>
2022-06-06 01:45:03 +0300

    Add files via upload

---
+++ practice7/generated/source/buildConfig/debug/ru/mirea/ivanova/socketconnection/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ru.mirea.ivanova.socketconnection;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ru.mirea.ivanova.socketconnection";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
